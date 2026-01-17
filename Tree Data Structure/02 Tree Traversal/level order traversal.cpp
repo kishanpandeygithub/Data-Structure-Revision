f556dc2c4f47037c79d28ea45c29634863bc661b
kishan pandey <kishanpandeym42@gmail.com>
2026-01-17 22:37:48 +0530

    third commit

--- Tree Data Structure/02 Tree Traversal/level order traversal.cpp
+++ Tree Data Structure/02 Tree Traversal/level order traversal.cpp
@@ -1,52 +1,93 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* left;
-    Node* right;
-    Node(int data){
-        this->data =data;
-        this->left =NULL;
-        this->right =NULL;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
-Node* create(){
+Node *create()
+{
     int data;
-    cout<<"\nEnter the Data : ";
-    cin>>data;
-    if(data==-1){
+    cout << "\nEnter the Data : ";
+    cin >> data;
+    if (data == -1)
+    {
         return NULL;
     }
-    Node* root = new Node(data);
-    //to insert the left node
-    cout<<"\nEnter the left Node fo the "<< root->data;
+    Node *root = new Node(data);
+    // to insert the left node
+    cout << "\nEnter the left Node fo the " << root->data;
     root->left = create();
-    //to insert the right node
-    cout<<"\nEnter the right Node fo the "<< root->data;
+    // to insert the right node
+    cout << "\nEnter the right Node fo the " << root->data;
     root->right = create();
     return root;
 }
-void levelOrderTraversal(Node* root){
-    queue<Node*> q;
+// this is the level ordet traversal that can traverse the node in one by one
+
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
     q.push(root);
-    while(!q.empty()){
-        Node* temp =q.front();
+    while (!q.empty())
+    {
+        Node *temp = q.front();
         q.pop();
-        cout<<temp->data<<" ";
-        if(temp->left){
+        cout << temp->data << " ";
+        if (temp->left)
+        {
             q.push(temp->left);
         }
-        if(temp->right){
+        if (temp->right)
+        {
             q.push(temp->right);
         }
     }
 }
 
-int main(){
-    Node * root =create();
+// this is the level order treaversal that traverse the node in the
+// seprtarion of the each level
+
+void levelOrderTraversalSeperate(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        /* code */
+        int n = q.size();
+        while (n--)
+        {
+            Node *temp = q.front();
+            q.pop();
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+        cout<<"\n";
+    }
+}
+int main()
+{
+    Node *root = create();
     levelOrderTraversal(root);
+    cout<<"\n";
+    levelOrderTraversalSeperate(root);
     return 0;
 }
